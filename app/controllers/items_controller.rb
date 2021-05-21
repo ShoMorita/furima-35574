a11c0699dd4d86ed86a252679d4ccf121b4cc4cc
ShoMorita <smorita0520@gmail.com>
2021-05-21 12:19:16 +0900

    deviseに関するビューファイルの配置

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,30 @@
 class ItemsController < ApplicationController
-  def index
+
+  def index 
+    @items = Items.all
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def updete
+  end
+
+  def destroy
+  end
+
+  private
+
+  
+
+
 end
