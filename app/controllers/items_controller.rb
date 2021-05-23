487f24a1cc096d8ef21819f0337b7d1f111d1dbc
ShoMorita <smorita0520@gmail.com>
2021-05-23 13:15:50 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,9 @@ class ItemsController < ApplicationController
 
   private
 
+  def item_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
   
-
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+  # belong_to :user
+  # has_one :buyer
+
+  has_one_attached :image
 end
