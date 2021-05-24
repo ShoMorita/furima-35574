042f745532c2ecbdd4ed3d104eefc4a4e86b7cc1
ShoMorita <smorita0520@gmail.com>
2021-05-24 19:35:32 +0900

    商品出品機能　修正２

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,24 +1,29 @@
 class Item < ApplicationRecord
-  belongs_to :user
-  has_one :buyer
+  # belongs_to :user
+  # has_one :buyer
 
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  validates :image, presence: true
-  validates :name, presence: true
-  validates :description, presence: true
-  belongs_to :category
-  validates :category_id, numericality: { other_than: 1 } 
-  belongs_to :status
-  validates :status_id, numericality: { other_than: 1 } 
-  belongs_to :delivery_charge
-  validates :delivery_charge_id, numericality: { other_than: 1 } 
-  belongs_to :prefecture
-  validates :prefecture_id, numericality: { other_than: 1 } 
-  belongs_to :shipping_day
-  validates :shipping_day_id, numericality: { other_than: 1 } 
-  validates :selling_price, presence: true
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :selling_price
+  end
 
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :delivery_charge_id
+    validates :prefecture_id
+    validates :shipping_day_id
+  end
+
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :shipping_day
   
 end
