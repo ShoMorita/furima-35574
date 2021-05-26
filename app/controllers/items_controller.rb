3b761fb52873909a77abd911d9a9c9ef040a984b
ShoMorita <smorita0520@gmail.com>
2021-05-26 11:21:49 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [ :new, :create ]
   before_action :set_item, only: [ :show ]
+  before_action :move_to_id, only: [:edit, :update ]
 
   def index
     @items = Item.order("created_at DESC")
@@ -28,7 +29,13 @@ class ItemsController < ApplicationController
   def edit
   end
 
-  def updete
+  def update
+    @item = Item.find(params[:id])
+    if  @item.update(item_params) 
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -42,6 +49,11 @@ class ItemsController < ApplicationController
   
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_id
+    @item = Item.find(params[:id])
+    redirect_to root_path unless current_user == @item.user
   end
 
 end
