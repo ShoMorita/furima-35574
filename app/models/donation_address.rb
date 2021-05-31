cd8e6c620ea39b43ce1b05dfc67726f79b3ad1da
ShoMorita <smorita0520@gmail.com>
2021-05-31 21:54:23 +0900

    商品購入機能　修正６

--- app/models/donation_address.rb
+++ app/models/donation_address.rb
@@ -1,16 +1,15 @@
 class DonationAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id,:buyer_id, :postal_code, :prefecture_id,:municipality, :address, :building_name, :phone_number, :token
-
-  validates :postal_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-  validates :municipality, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-  validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/ } 
+  attr_accessor :user_id, :item_id,:buyer_id, :postal_code, :prefecture_id,:municipality, :address, :building_name, :phone_number, :token 
 
   with_options presence: true do
     validates :user_id
     validates :item_id
     validates :address
     validates :token
+    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :municipality, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   end
 
   with_options numericality: { other_than: 1 } do
